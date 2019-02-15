2761746e6c87b43f0cdf4dc6a69a64e7a125ab5a
thelazarbeam <eddie.lazar@yahoo.com>
2019-02-14 20:42:51 -0500

    refactored constants into importable file

--- game.py
+++ game.py
@@ -6,30 +6,7 @@
 import pygame
 import pygame.locals
 from time import sleep
-
-#############################################################
-### CONSTANTS ###
-#############################################################
-
-red =  (255,   0,   0)
-teal = (  0, 128, 128)
-brown =(128, 110,  98)
-white =(255, 255, 255)
-
-BLOCKSIZE = 20
-BOARD_X = 50
-BOARD_Y = 50
-PLAYER_MVMT = BLOCKSIZE/10
-
-#############################################################
-### HELPER METHODS ###
-#############################################################
-
-def quitCheck():
-	if pygame.key.get_pressed()[pygame.K_q]:
-		exit()
-	for event in pygame.event.get():
-		if event.type == pygame.locals.QUIT: exit()
+from resources import *
 
 #############################################################
 ### SUB-CLASSES ###
---
+++ resources.py
@@ -0,0 +1,35 @@
+
+import pygame
+import pygame.locals
+
+#############################################################
+### CONSTANTS ###
+#############################################################
+
+red =  (255,   0,   0)
+teal = (  0, 128, 128)
+brown =(128, 110,  98)
+white =(255, 255, 255)
+
+BLOCKSIZE = 20
+BOARD_X = 50
+BOARD_Y = 50
+PLAYER_MVMT = BLOCKSIZE/10
+
+
+#############################################################
+### HELPER METHODS ###
+#############################################################
+
+def quitCheck():
+	if pygame.key.get_pressed()[pygame.K_q]:
+		exit()
+	for event in pygame.event.get():
+		if event.type == pygame.locals.QUIT: exit()
+
+
+
+
+####
+if __name__ == "__main__":
+	print("Dont run the resources file ya dimbis")
