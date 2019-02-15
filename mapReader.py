96ad111192330141253f680fa778dce572702f02
thelazarbeam <eddie.lazar@yahoo.com>
2019-02-14 23:42:27 -0500

    needed a script to build saved maps. WIP

---
+++ mapReader.py
@@ -0,0 +1,32 @@
+
+from resources import *
+import pygame
+		
+try:        
+	f = open(input("Which board? >>")+".csv", "r")
+except FileNotFoundError:
+	print("no board by that name here")
+	exit()    
+
+lines = f.readlines()
+f.close()
+
+
+screen = pygame.display.set_mode((BLOCKSIZE*BOARD_X, BLOCKSIZE*BOARD_Y))
+screen.fill(white)
+
+for i in range(len(lines)):
+	vals = lines[i].split(",")
+	for j in range(len(vals)):
+		if int(vals[j]) == 1:
+			x = pygame.Surface((BLOCKSIZE, BLOCKSIZE))
+			x.fill(brown)
+			screen.blit(x, (j*BLOCKSIZE, i*BLOCKSIZE))
+
+
+pygame.display.flip()
+		
+
+while 1:
+	if quitCheck():
+		break
